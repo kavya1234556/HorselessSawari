a181a4fbdaeb509730d41ddf3df352554119bb2f
kavya123456 <kavya.timilsina1230@gmail.com>
2024-02-23 23:19:52 +0545

    api doc setup compelete

--- horseless-sawari/app/api/car/category/route.ts
+++ horseless-sawari/app/api/car/category/route.ts
@@ -1,6 +1,14 @@
 import { db } from '@/lib/db';
 import { NextResponse } from 'next/server';
-
+/**
+ * @swagger
+ * /api/car/category:
+ *   get:
+ *     description: return all the category(manufacture available)
+ *     responses:
+ *       200:
+ *         description: using this in car-hosting-page for adding vehicle manufacture list!
+ */
 export async function GET(req: Request) {
   try {
     const Category = await db.category.findMany();
